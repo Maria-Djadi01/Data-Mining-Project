94ca69fa63319f11c0edfa609d7e9eac64cc0106
dounia4112 <dounia4112@gmail.com>
2023-11-08 12:17:21 +0100

    make datasets

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+from glob import glob
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+# --------------------------------------------------------------
+# Read  Static file
+# --------------------------------------------------------------
+
+file_id = "1zcNeHEEoi9XmpPYcUKTqRB_VngeQGZEK"
+
+download_link = f"https://drive.google.com/uc?id={file_id}"
+
+static_dataset_df = pd.read_csv(download_link)
+static_dataset_df.to_csv("../../data/raw/static_dataset.csv")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+# --------------------------------------------------------------
+# Read  Time Series file
+# --------------------------------------------------------------
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+file_id = "1CQMBEUzL8g39Cpost4D5_6a_mraOlFNP"
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+download_link = f"https://drive.google.com/uc?id={file_id}"
 
-    main()
+tempo_dataset_df = pd.read_csv(download_link)
+tempo_dataset_df.to_csv("../../data/raw/temp_dataset.csv")
