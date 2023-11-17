2da6c8a5a8624b87fc7bd9978e60f633ca68775e
Maria-Djadi01 <maria.djadi01@gmail.com>
2023-11-17 17:03:41 +0100

    add dataset2_TSA_2

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -8,6 +8,7 @@ sys.path.append(project_dir)
 
 import pandas as pd
 from src.utils import central_tendances
+import numpy as np
 
 
 # --------------------------------------------------------------
@@ -33,7 +34,7 @@ file_id = "1CQMBEUzL8g39Cpost4D5_6a_mraOlFNP"
 # tempo_dataset_df_processed = pd.read_csv(download_link)
 # tempo_dataset_df_processed.to_csv("../../data/raw/temp_dataset.csv")
 
-tempo_dataset_df_processed = pd.read_csv("../../data/raw/temp_dataset.csv")
+tempo_dataset_df_processed = pd.read_csv("../../data/raw/temp_dataset.csv", index_col=0)
 
 # convert the start date and end date objects to date object, by exploring the
 # whole df we noticed two types of dates. The first one is like '10/31/2020'
@@ -41,15 +42,14 @@ tempo_dataset_df_processed = pd.read_csv("../../data/raw/temp_dataset.csv")
 # object.
 
 # The issue with the second format is that it doesn't have the year, so we need
-# to add it. 
+# to add it.
 
 df_time_fixed = tempo_dataset_df_processed.copy()
 # Filter rows with and without a '/' in the 'Start date' column
-filtered_df_with_year = df_time_fixed[df_time_fixed["Start date"].str.contains(
-    "/")]
-filtered_df_without_year = df_time_fixed[~df_time_fixed["Start date"].str.contains("/")][
-    ["Start date", "end date", "time_period"]
-]
+filtered_df_with_year = df_time_fixed[df_time_fixed["Start date"].str.contains("/")]
+filtered_df_without_year = df_time_fixed[
+    ~df_time_fixed["Start date"].str.contains("/")
+][["Start date", "end date", "time_period"]]
 
 # Convert the 'Start date' column to datetime format using .loc
 filtered_df_with_year.loc[:, "year"] = pd.to_datetime(
@@ -57,8 +57,9 @@ filtered_df_with_year.loc[:, "year"] = pd.to_datetime(
 ).dt.year
 
 # Select the relevant columns
-start_date_column_with_year = filtered_df_with_year[[
-    "year", "Start date", "end date", "time_period"]]
+start_date_column_with_year = filtered_df_with_year[
+    ["year", "Start date", "end date", "time_period"]
+]
 
 for index, row in filtered_df_without_year.iterrows():
     if row["time_period"] not in start_date_column_with_year["time_period"].values:
@@ -70,23 +71,56 @@ for index, row in filtered_df_without_year.iterrows():
     ].iloc[0]
 
     df_time_fixed["Start date"].loc[index] = pd.to_datetime(
-        df_time_fixed["Start date"].loc[index] + '-' + str(int(year)))
+        df_time_fixed["Start date"].loc[index] + "-" + str(int(year))
+    )
     df_time_fixed["end date"].loc[index] = pd.to_datetime(
-        df_time_fixed["end date"].loc[index] + '-' + str(int(year)))
+        df_time_fixed["end date"].loc[index] + "-" + str(int(year))
+    )
 
 for index, row in filtered_df_with_year.iterrows():
-    df_time_fixed['Start date'].loc[index] = pd.to_datetime(
-        df_time_fixed['Start date'].loc[index])
-    df_time_fixed['end date'].loc[index] = pd.to_datetime(
-        df_time_fixed['end date'].loc[index])
+    df_time_fixed["Start date"].loc[index] = pd.to_datetime(
+        df_time_fixed["Start date"].loc[index]
+    )
+    df_time_fixed["end date"].loc[index] = pd.to_datetime(
+        df_time_fixed["end date"].loc[index]
+    )
+
+df_time_fixed["Start date"] = pd.to_datetime(df_time_fixed["Start date"])
+df_time_fixed["end date"] = pd.to_datetime(df_time_fixed["end date"])
+
+# keep only the dates
+df_time_fixed["Start date"] = df_time_fixed["Start date"].dt.date
+df_time_fixed["end date"] = df_time_fixed["end date"].dt.date
 
 tempo_dataset_df_processed = df_time_fixed
 
+
+
+# convert zcta to str
+tempo_dataset_df_processed["zcta"] = tempo_dataset_df_processed["zcta"].astype(int)
+
+tempo_dataset_df_processed.info()
+
+# Calculate the midpoint
+tempo_dataset_df_processed["Midpoint Date"] = (
+    tempo_dataset_df_processed["Start date"]
+    + (
+        tempo_dataset_df_processed["end date"]
+        - tempo_dataset_df_processed["Start date"]
+    )
+    / 2
+)
+
+# Set the midpoint as the index
+tempo_dataset_df_processed.set_index('Midpoint Date', inplace=True, drop=True)
+
+# Drop the 'Start date' and 'End date' columns if needed
+tempo_dataset_df_processed.drop(["Start date", "end date"], axis=1, inplace=True)
+
+# To improve data readability, we will reorder columns and the rows according
+# to Start date and the state zip code
 column_order = [
     "zcta",
-    "time_period",
-    "Start date",
-    "end date",
     "population",
     "test count",
     "positive tests",
@@ -95,10 +129,7 @@ column_order = [
     "case rate",
     "positivity rate",
 ]
-
-# To improve data readability, we will reorder columns and the rows according
-# to Start date and the state zip code
-tempo_dataset_df_processed = tempo_dataset_df_processed[column_order].sort_values(by=["zcta", "Start date"])
-# turn zcta into object
-
+tempo_dataset_df_processed.sort_values(by=["Midpoint Date", "zcta"], inplace=True)
+tempo_dataset_df_processed = tempo_dataset_df_processed[column_order]
+tempo_dataset_df_processed.head()
 tempo_dataset_df_processed.to_csv("../../data/interim/temp_dataset_processed.csv")
---
+++ src/features/data2/build_features.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
+
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+
+# ----------------------------------------------------------------#
+# Load data #
+# ----------------------------------------------------------------#
+
+df = pd.read_csv("../../../data/interim/temp_dataset_processed.csv", index_col=0)
+
+# --------------------------------------------------------------
+# Dealing with missing values (imputation)
+# --------------------------------------------------------------
+
+# Number of missing values in each column
+for col in df.columns:
+    print(col, df[col].isnull().sum())
+
+# We can fill the missing values in test count by looking to test rate if we know the population
+df_fill_test_count = df.copy()
+for row in df_fill_test_count[df_fill_test_count["test count"].isnull()].index:
+    df_fill_test_count.loc[row, "test count"] = (
+        df_fill_test_count.loc[row, "test rate"]
+        * df_fill_test_count.loc[row, "population"]
+        / 100
+    ).astype(int)
+df_fill_test_count["test count"].isnull().sum()
+
+# We can fill the positive tests by looking to test count and positivity rate
+df_fill_positive_tests = df_fill_test_count.copy()
+for row in df_fill_positive_tests[
+    df_fill_positive_tests["positive tests"].isnull()
+].index:
+    df_fill_positive_tests.loc[row, "positive tests"] = (
+        df_fill_positive_tests.loc[row, "test count"]
+        * df_fill_positive_tests.loc[row, "positivity rate"]
+        / 100
+    ).astype(int)
+df_fill_positive_tests["positive tests"].isnull().sum()
+
+# We can fill the case count by looking at the case count and population
+df_fill_case_count = df_fill_positive_tests.copy()
+for row in df_fill_case_count[df_fill_case_count["case count"].isnull()].index:
+    df_fill_case_count.loc[row, "case count"] = (
+        df_fill_case_count.loc[row, "population"]
+        * df_fill_case_count.loc[row, "case rate"]
+        / 100
+    ).astype(int)
+df_fill_case_count["case count"].isnull().sum()
+
+df_fill_miss = df_fill_case_count
+for col in df_fill_miss.columns:
+    print(col, df_fill_miss[col].isnull().sum())
+
+# ----------------------------------------------------------------#
+# Save file
+# ----------------------------------------------------------------#
+df_fill_miss.to_csv("../../../data/interim/02_temp_dataset_fill_miss_val.csv")
