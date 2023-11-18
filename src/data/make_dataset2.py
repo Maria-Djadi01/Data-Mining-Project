19c8e0dadb30b3f81aa0b8a99b8852b3fad44cf7
Maria-Djadi01 <maria.djadi01@gmail.com>
2023-11-18 09:27:26 +0100

    update make_dataset

--- src/data/make_dataset2.py
+++ src/data/make_dataset2.py
@@ -1,27 +1,14 @@
 import sys
 
 # Specify the directory where your data is located
-project_dir = "D:/2M/D.Mining/Data-Mining-Project/"
+# project_dir = "D:/2M/D.Mining/Data-Mining-Project/"
 
 # Change the working directory
-sys.path.append(project_dir)
+# sys.path.append(project_dir)
 
 import pandas as pd
 from src.utils import central_tendances
 import numpy as np
-
-
-# --------------------------------------------------------------
-# Read  Static file
-# --------------------------------------------------------------
-
-file_id = "1zcNeHEEoi9XmpPYcUKTqRB_VngeQGZEK"
-
-# download_link = f"https://drive.google.com/uc?id={file_id}"
-
-# static_dataset_df = pd.read_csv(download_link)
-# static_dataset_df.to_csv("../../data/raw/static_dataset.csv")
-
 
 # --------------------------------------------------------------
 # Read  Time Series file
